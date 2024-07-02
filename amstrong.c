75de4a471d8172af602f18da1c492f688f155bf6
Navaneeth_PV <153710420+navaneethpv@users.noreply.github.com>
2024-07-02 01:06:32 -0400

    Add files via upload

---
+++ amstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int num,og,reminder,ams=0;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    og=num;
+
+    while(num!=0)
+    {
+        reminder=num%10;
+        ams=ams+(reminder*reminder*reminder);
+        num/=10;
+    }
+    
+    if(og==ams)
+    {
+        printf("\n The number %d is amstrong!",og);
+    }
+    else
+    {
+        printf("\n The number %d is not amstrong!");
+    }
+}
